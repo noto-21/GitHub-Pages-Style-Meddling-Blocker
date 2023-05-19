986ee902505e2bb0925689e8c12512d463f11760
NT <128177967+noto-21@users.noreply.github.com>
2023-05-18 22:17:55 -0400

    Update GPIBlock.js

--- GPIBlock.js
+++ GPIBlock.js
@@ -2,7 +2,7 @@ document.addEventListener/*Listen for DOM Loading*/('DOMContentLoaded', () =>
 {
     const observer = new MutationObserver/*Observe mutations to DOM*/(() => 
     {
-        const styleElement = document.querySelector('style[data-styled]');//The change in question is the addition of a style tag with the attribute data-styled
+        const styleElement = document.querySelector('style[data-styled]');//The change in question is the addition of a style tag with the attribute 'data-styled'
         if (styleElement)//If it exists, remove it
         {
             styleElement.remove();
